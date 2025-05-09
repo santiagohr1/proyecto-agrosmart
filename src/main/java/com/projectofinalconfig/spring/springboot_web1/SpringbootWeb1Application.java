1150a2d8f7673debe8cf60bb23a6d47a2b1321b0
Santiago <santiagohidalgor@gmail.com>
2025-05-09 17:26:33 -0500

    Primer commit

---
+++ src/main/java/com/projectofinalconfig/spring/springboot_web1/SpringbootWeb1Application.java
@@ -0,0 +1,13 @@
+package com.projectofinalconfig.spring.springboot_web1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectofinalconfig/spring/springboot_web1/SpringbootWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectofinalconfig.spring.springboot_web1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
